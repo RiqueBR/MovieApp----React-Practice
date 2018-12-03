0119e62d392df3fe9db271a1bed2c1e41c13c34e
Henrique Pereira Batista <h.p.batista20@gmail.com>
2018-12-03 22:44:26 +0000

    styles added

--- src/Components/button.js
+++ src/Components/button.js
@@ -4,7 +4,7 @@ class ImdbButton extends React.Component{
 
     render(){
         return (
-             <p><a href = "https://www.imdb.com/"> View More Updoming Releases</a></p>
+             <p><a href="https://www.imdb.com/"> View More Updoming Releases</a></p>
         )
        
     }
